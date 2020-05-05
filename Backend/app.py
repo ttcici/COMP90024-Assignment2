92b2f93469bae13282c4b403592188a44c8f32cb
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-05-05 21:31:24 +0800

    add requests

--- Backend/app.py
+++ Backend/app.py
@@ -1,16 +1,22 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
-def login():
+def demo():
     if request.method == 'POST':
         data = request.get_json()
         print(data)
         return jsonify({
             "username": 'demo0'
         })
+
+@app.route('/test', methods=['POST'])
+def test():
+    if request.method == 'POST':
+        r = request.post
 
 if __name__ == '__main__':
     app.run()
