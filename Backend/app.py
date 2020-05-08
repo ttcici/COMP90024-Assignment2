361cdc94fe568222b9dc469b8256494e8ed9039a
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-05-08 22:54:00 +0800

    update

--- Backend/app.py
+++ Backend/app.py
@@ -1,3 +1,8 @@
+# This is the python program to run flask as the back-end.
+# Functionality: Get data from CouchDB as front-end requests, send the data back to front-end.
+# Version: 1.0
+# Last-update: 8/5/2020
+
 import json
 from flask import Flask, request, jsonify
 import requests
@@ -5,20 +10,18 @@ import couchdb
 
 app = Flask(__name__)
 
+# TODO: set CouchDB url
+# url =
 
-@app.route('/', methods=['POST'])
-def demo():
-    data = request.get_json()
-    print(data)
-    return jsonify({
-        "username": 'demo0'
-    })
-
-
-@app.route('/', methods=['GET'])
-def demo1():
-    r = requests.post("http://127.0.0.1:5555/", json.dumps({"1": "2"}))
-    return r.json()
+# Get doc from database by name
+@app.route('/<string:name>', methods=['GET'])
+def demo1(name):
+    # r = requests.post("http://127.0.0.1:5555/", json.dumps({"1": "2"}))
+    # return r.json()
+    server = couchdb.Database()
+    cdb = server['Databse name']
+    doc = cdb[name]
+    return jsonify(doc)
 
 if __name__ == '__main__':
     app.run()
--- Backend/retrieve_tweets.py
+++ Backend/retrieve_tweets.py
@@ -2,6 +2,7 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import couchdb
 
 # Enter Twitter API Keys
 access_token = "1254430088793223168-2vNjv4WYXrfkvAnwxpmYvXDeJiL2SI"
@@ -9,11 +10,6 @@ access_token_secret = "NjGEh6pK6oUru4WwndDzDdrIgms7zn5M8LY52Ryg1HQGK"
 consumer_key = "be5MWgU3xxhxL3yXBW5P84vtL"
 consumer_secret = "7KLLsiXLsR71arTnBwQNc14EDaE7UZrpsepINb4Q8UauLOjWb8"
 
-# Enter Twitter API Keys
-#consumer_key=
-#consumer_secret=
-#access_token=
-#access_token_secret=
 
 # Create the class that will handle the tweet stream
 class StdOutListener(StreamListener):
