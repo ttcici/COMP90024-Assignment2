58447da4bbce89980b1a98591a9fe50c235e42be
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-05-09 15:57:44 +0800

    init structure

--- Backend/utils.py
+++ Backend/utils.py
@@ -3,8 +3,10 @@
 # Last-update: 9/5/2020
 
 import couchdb
+from tweepy import StreamListener
 
 
+# Connect CouchDB
 class DatabaseConnection:
     # TODO: set CouchDB url & database name
     url = "172.26.0.0"
@@ -16,3 +18,14 @@ class DatabaseConnection:
 
     def get_db(self):
         return self.server[self.db_name]
+
+
+# Create the class that will handle the tweet stream
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
