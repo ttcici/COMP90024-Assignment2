6b88e7c197d670459820b5f6e17b46d59f0a6967
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-05-16 21:08:35 +0800

    Update stream_tweet_thread.py

--- Backend/stream_tweet_thread.py
+++ Backend/stream_tweet_thread.py
@@ -4,7 +4,7 @@ from tweepy import Stream
 from threading import Thread
 import json
 
-count = 0
+# count = 0
 
 
 class Get(Thread):
@@ -14,22 +14,25 @@ class Get(Thread):
         self.auth = auth
         self.query = query
         self.couchdb = couchdb
-        self.total_number = total_number
-        self.listener = StdOutListener(self.couchdb)
+        # self.total_number = total_number
+        self.listener = StdOutListener(self.couchdb, total_number)
 
 
     def run(self):
         global count
-        while count < self.total_number:
-            stream = Stream(self.auth, self.listener)
-            stream.filter(track=self.query)
-
+        # while count < self.total_number:
+        #     stream = Stream(self.auth, self.listener)
+        #     stream.filter(track=self.query)
+        stream = Stream(self.auth, self.listener)
+        stream.filter(track=self.query)
 
 # Create the class that will handle the tweet stream
 class StdOutListener(StreamListener):
-    def __init__(self, counchdb):
+    def __init__(self, counchdb, total_number):
         super().__init__()
         self.couchdb = counchdb
+        self.total_number = total_number
+        self.count = 0
 
     def on_data(self, data):
         json_data = json.loads(data)
@@ -38,9 +41,12 @@ class StdOutListener(StreamListener):
         # Check duplicate
         if self.couchdb.get(json_data['id_str']) is None:
             # Save tweet to CouchDB
-            global count
-            count += 1
-            self.couchdb[json_data['id_str']] = json_data
+            # global count
+            if self.count < self.total_number:
+                self.count += 1
+                self.couchdb[json_data['id_str']] = json_data
+            else:
+                return False
 
     def on_error(self, status):
         print(status)
