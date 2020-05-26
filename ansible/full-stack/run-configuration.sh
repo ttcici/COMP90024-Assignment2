800092b3e33a5ee8316117a07198b78591b50b10
Xu Qingmeng <qingmeng.xu@student.unimelb.edu.au>
2020-05-27 03:18:21 +1000

    delete useless code

--- ansible/full-stack/run-configuration.sh
+++ ansible/full-stack/run-configuration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-. ./unimelb-comp90024-2020-grp-16-openrc.sh; ansible-playbook -i hosts --ask-become-pass contiguration.yaml
+. ./unimelb-comp90024-2020-grp-16-openrc.sh; ansible-playbook -i hosts --ask-become-pass configuration.yaml
