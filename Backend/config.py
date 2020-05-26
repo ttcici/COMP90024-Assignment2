b812575d9670f2392faceedd59edd9b37fe05f90
xsqc2 <49745741+xsqc2@users.noreply.github.com>
2020-05-26 11:29:24 +0800

    add team info

--- Backend/config.py
+++ Backend/config.py
@@ -1,3 +1,11 @@
+# Team 16: COMP90024-Assignment2
+# Team Members:
+# Qingmeng Xu, 969413
+# Tingqian Wang, 1043988
+# Zhong Liao, 1056020
+# Cheng Qian, 962539
+# Zongcheng Du, 1096319
+
 # Enter Twitter API Keys
 access_token = "1254430088793223168-2vNjv4WYXrfkvAnwxpmYvXDeJiL2SI"
 access_token_secret = "NjGEh6pK6oUru4WwndDzDdrIgms7zn5M8LY52Ryg1HQGK"
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Team 16: COMP90024-Assignment2
+﻿# Team 16: COMP90024-Assignment2
 ## Team Members
 ```
 Qingmeng Xu, 969413
 Tingqian Wang, 1043988
 Zhong Liao, 1056020
 Cheng Qian, 962539
-Zongcheng Du, xxxxxxx
+Zongcheng Du, 1096319
 ```
 
 ## Video Links
